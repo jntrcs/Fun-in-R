8f53a69536528e59377e65270e2f58baebaba9df
Jackson Curtis <jcurtis210@gmail.com>
2016-07-28 17:10:53 -0600

    Two simple simulations--gems and baseball teams

---
+++ monstergems.R
@@ -0,0 +1,25 @@
+oneOfEach<-function()
+{
+  monsters<-0
+  gems<-c(0,0,0)
+  while(0%in%gems)
+  {
+    whichGem<-sample(c(1,2,3), 1, prob=c(3,2,1))
+    gems[whichGem]<-gems[whichGem]+1
+  }
+  gems[1]
+}
+
+simul<-sapply(1:100000, FUN=function(i) oneOfEach())
+hist(simul)
+mean(simul)
+table(simul)/length(simul)
+max(simul)
+sum(simul[simul==1])/10000
+
+#Baseball
+bsimul<-sapply(1:500000, FUN = function(i) max(rbinom(5,162,.5)))
+hist(bsimul)
+max(bsimul)
+min(bsimul)
+mean(bsimul)
