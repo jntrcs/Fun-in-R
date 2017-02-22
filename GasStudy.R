52b4ffcf6016e9bde5ed1eaf58295a733caaef46
Jackson Curtis <jcurtis210@gmail.com>
2017-02-21 21:19:35 -0700

    added a roadtrip variable

--- GasStudy.R
+++ GasStudy.R
@@ -6,16 +6,17 @@ dat<-dat[dat$Filled.Up=="Full",]
 dat$Cost.Gallon<- as.numeric(sub('\\$','',as.character(dat$Cost.Gallon)))
 dat$Date<-as.Date(as.character(dat$Date))
 dat$Odometer<-as.numeric(sub(',','',dat$Odometer))
+dat$RoadTrip<-dat$Location %in% c("Colorado", "Blanding", "Laramie")
+dat$RoadTrip[13]<-T
+dat$RoadTrip[37]<-T
 plot(dat$Odometer~dat$Date)
 dat$DaysSinceLastFillUp<-c(dat$Date[-length(dat$Date)]-dat$Date[-1], NA)
 
-model<-lm(dat$MPG~dat$Gas.Brand+dat$DaysSinceLastFillUp)
+model<-lm(dat$MPG~dat$Gas.Brand+dat$DaysSinceLastFillUp+dat$RoadTrip)
 plot(model$residuals, col=dat$Gas.Brand)
-abline(v=c(dat$Date), col="Black")
-abline(v=c(dat$Date[dat$Gas.Brand=="Maverick"]), col="red")
-abline(v=c(dat$Date[dat$Gas.Brand=="Costco"]), col="blue")
+summary(model)
 
-dat$Odometer<-as.numeric(sub(',', '', as.character(dat$Odometer)))
+
 dat$MPG
 aggregate(dat$MPG~dat$Gas.Brand, FUN=function(i)c(mean(i), length(i)))
 hist(dat$MPG[dat$Gas.Brand=="Maverick"])
